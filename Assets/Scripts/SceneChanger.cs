8a7088de7dbb3c1ccae1e871896283a19d520a81
Nalien <211351701+NalienDev@users.noreply.github.com>
2026-04-07 19:15:36 +0100

    Attempt to fix between-scene voice chat

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,5 +1,7 @@
+using JamesFrowen.SimpleWeb;
 using PurrNet;
 using PurrNet.Transports;
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -15,17 +17,52 @@ public class SceneChanger : NetworkIdentity
         Instance = this;
         DontDestroyOnLoad(gameObject);
         _networkManager.onServerConnectionState += OnServerState;
+        _networkManager.onClientConnectionState += OnClientState;
+    }
+
+    private void OnSceneLoaded(SceneID scene, bool asServer)
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "GameLobby")
+        {
+            var bridge = FindFirstObjectByType<Dissonance.Integrations.PurrNet.PurrNetCommsNetwork>();
+
+            if (bridge != null)
+            {
+                PurrNet.Logging.PurrLogger.Log($"Lobby Scene Loaded. Manually starting Dissonance. AsServer: {asServer}");
+
+                bridge.TryRunManually();
+            }
+            else
+            {
+                PurrNet.Logging.PurrLogger.LogError("Could not find PurrNetCommsNetwork in GameLobby!");
+            }
+        }
     }
 
     protected override void OnDestroy()
     {
         _networkManager.onServerConnectionState -= OnServerState;
+        _networkManager.onClientConnectionState -= OnClientState;
+        _networkManager.sceneModule.onSceneLoaded -= OnSceneLoaded;
+
     }
+
+    private void OnClientState(ConnectionState state)
+    {
+        if (state == ConnectionState.Connected)
+        {
+            _networkManager.sceneModule.onSceneLoaded += OnSceneLoaded;
+        }
+    }
+
 
     private void OnServerState(ConnectionState state)
     {
         if (state == ConnectionState.Connected)
+        {
             _networkManager.sceneModule.LoadSceneAsync("GameLobby");
+            _networkManager.sceneModule.onSceneLoaded += OnSceneLoaded;
+        }
     }
 
     [ServerRpc(requireOwnership: false)]
