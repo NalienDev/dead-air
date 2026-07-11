a641ed4677c5153f7843d4b3409f3bb868a78b34
afonsosantos-28 <afonso.santos1227@gmail.com>
2026-07-11 18:18:21 +0100

    add Fixing Stun; add Timer Lever

--- Assets/Scripts/Rover/Sucker.cs
+++ Assets/Scripts/Rover/Sucker.cs
@@ -7,7 +7,10 @@ public class Sucker : NetworkBehaviour
     [SerializeField] private SuctionZone _suctionZone;
     [SerializeField] private Animator _leverAnimator;
     [SerializeField] private string _animatorParameterName = "isPulled";
+    [SerializeField] private float _vacuumDuration = 3f;
+    
     private SyncVar<bool> _canSuck = new SyncVar<bool>(false);
+    private Coroutine _timerCoroutine;
 
     protected override void OnSpawned(bool asServer)
     {
@@ -15,18 +18,41 @@ public class Sucker : NetworkBehaviour
         SetZoneActive(_canSuck.value);
     }
 
-    protected override void OnDespawned(bool asServer) => _canSuck.onChanged -= SetZoneActive;
+    protected override void OnDespawned(bool asServer)
+    {
+        _canSuck.onChanged -= SetZoneActive;
+        if (asServer && _timerCoroutine != null)
+        {
+            StopCoroutine(_timerCoroutine);
+            _timerCoroutine = null;
+        }
+    }
 
     public bool CanSuck() => _canSuck.value;
 
-    public void SetCanSuck(bool value)
+    public void ActivateVacuum()
     {
-        if (isServer) _canSuck.value = value;
-        else ServerSetCanSuck(value);
+        if (isServer) ServerActivateVacuum();
+        else ServerRequestActivate();
     }
 
     [ServerRpc(requireOwnership: false)]
-    private void ServerSetCanSuck(bool value) => _canSuck.value = value;
+    private void ServerRequestActivate() => ServerActivateVacuum();
+
+    private void ServerActivateVacuum()
+    {
+        if (!isServer) return;
+        if (_timerCoroutine != null) StopCoroutine(_timerCoroutine);
+        _canSuck.value = true;
+        _timerCoroutine = StartCoroutine(VacuumTimerRoutine());
+    }
+
+    private System.Collections.IEnumerator VacuumTimerRoutine()
+    {
+        yield return new WaitForSeconds(_vacuumDuration);
+        _canSuck.value = false;
+        _timerCoroutine = null;
+    }
 
     private void SetZoneActive(bool value)
     {
